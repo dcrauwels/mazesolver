9a870b233e4a7af521266bb17f5614fc556ea54f
dcrauwels <d.crauwels@gmail.com>
2024-02-18 11:46:59 +0100

    Cell class

--- main.py
+++ main.py
@@ -46,16 +46,38 @@ class Line:
         canvas.pack(fill=BOTH, expand = 1)
 
 class Cell:
-    def __init__(self, lw = True, rw = True, tw = True, bw = True, x, y, window) -> None:
+    def __init__(
+            self, window, xy_tl, xy_br, 
+            lw = True, rw = True, tw = True, bw = True
+            ) -> None:
         self.has_left_wall = lw
         self.has_right_wall = rw
         self.has_top_wall = tw
         self.has_bottom_wall = bw
-        self._x1 = x
-        self._x2 = x+10
-        self._y1 = y
-        self._y2 = y+10
+        self._x1= xy_tl[0]
+        self._x2 = xy_br[0]
+        self._y1 = xy_tl[1]
+        self._y2 = xy_br[1]
         self._win = window
+
+    def draw(self):
+        walls = []
+
+        if self.has_left_wall:
+            walls.append(
+                    Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+        if self.has_right_wall:
+            walls.append(
+                    Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+        if self.has_top_wall:
+            walls.append(
+                    Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        if self.has_bottom_wall:
+            walls.append(
+                    Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
+
+        for w in walls:
+            self._win.draw_line(w, "black")
 
 def main():
     win = Window(800,600)
@@ -63,6 +85,13 @@ def main():
     l2 = Line(Point(600, 100), Point(100, 400))
     win.draw_line(l1, "blue")
     win.draw_line(l2, "red")
+    
+    c1 = Cell(win, (40, 40), (60,60), False, True, True, True)
+    c2 = Cell(win, (80, 40), (120, 80), True, False, True, False)
+    c1.draw()
+    c2.draw()
+
+    # dont touch this
     win.wait_for_close()
 
 
