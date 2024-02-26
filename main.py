c41b4acb41609d3f603462c15fad6e9d3843bd87
dcrauwels <d.crauwels@gmail.com>
2024-02-26 16:03:54 +0100

    Start of the wall breaking algorithm. Needs much work still.

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ class Cell:
         self.has_right_wall = rw
         self.has_top_wall = tw
         self.has_bottom_wall = bw
+        self._visited = False
         self._x1= xy_tl[0]
         self._x2 = xy_br[0]
         self._y1 = xy_tl[1]
@@ -70,26 +71,29 @@ class Cell:
         self._win = win
 
     def draw(self):
-        walls = []
+        #Draw cell by making four walls in walls list.
+        #Draws either background color or black based on has_x_wall
 
-        if self.has_left_wall:
-            walls.append(
-                    Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
-        if self.has_right_wall:
-            walls.append(
-                    Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
-        if self.has_top_wall:
-            walls.append(
-                    Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
-        if self.has_bottom_wall:
-            walls.append(
-                    Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
+        walls = []
+        wall_existence = [self.has_left_wall, self.has_right_wall, 
+                self.has_top_wall, self.has_bottom_wall]
+
+        walls.append(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+        walls.append(
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+        walls.append(
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        walls.append(
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
 
         if self._win is not None:
-            for w in walls:
-                self._win.draw_line(w, "black")
+            for i in range(4):
+                self._win.draw_line(walls[i], "black" if wall_existence[i] else "#d9d9d9")
     
     def draw_move(self, to_cell, undo=False):
+        #Draws a line from one cell to target to_cell.
+        #Uses middle of cell based on cell data members x1 x2 y1 y2
         own_middle_x = (self._x1 + self._x2) / 2
         own_middle_y = (self._y1 + self._y2) / 2
         target_middle_x = (to_cell._x1 + to_cell._x2) / 2
@@ -105,8 +109,20 @@ class Cell:
         cell_line_color = "red" if undo else "gray"
 
         self._win.draw_line(cell_line, cell_line_color)
+    
+    def _break_walls_r(self, i, j, seed = None):
+        #Recursively break walls to make a maze.
+        import random
+        if seed is not None:
+            random.seed(seed)
+
+        self._visited = True
+        while True:
+            ij_list = []
+
         
 class Maze:
+    #Init has a bunch of parameters plus call to local function _create_cells()
     def __init__(
             self,
             x1,
@@ -127,6 +143,8 @@ class Maze:
         self._create_cells()
 
     def _create_cells(self):
+        #Create empty cells array and then populate with cells.
+        #x and y coords are based on Wall parameters (starting x1, y1 and cell size)
         self._cells = []
         for c in range(self.num_cols):
 
@@ -138,16 +156,29 @@ class Maze:
 
             self._cells.append(column)
         self._animate()
+        self._break_entrance_and_exit()
 
     def _animate(self):
+        #Draw cells one by one (nested loop)
         for col in self._cells:
             for cel in col:
                 cel.draw()
                 if self._win is not None:
                     self._win.redraw()
                 from time import sleep
+                #Sleep, may want to comment this out later.
                 sleep(0.05)
 
+    def _break_entrance_and_exit(self):
+        #Break top wall in top left and bottom wall in bottom right cell
+        self._cells[0][0].has_top_wall = False
+        self._cells[-1][-1].has_bottom_wall = False
+
+        #Redraw
+        self._cells[0][0].draw()
+        self._cells[-1][-1].draw()
+
+        
 
 def main():
     win = Window(800,600)
